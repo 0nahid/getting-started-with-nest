d1a18f9c22429c9c7fe14e01ac50ea69e209e7ef
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-16 17:30:04 +0600

    module & readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './Auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BoookmarkModule } from './boookmark/boookmark.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BoookmarkModule],
 })
 export class AppModule {}
